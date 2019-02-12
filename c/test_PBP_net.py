ee20b7fc917f82c7198e1c6e3b9a3ef88c436014
Jakob <jakob.lindqvist@chalmers.se>
2019-02-12 13:27:22 +0100

    Python3 port
    Functional changes:
      Minor print's and local module imports to comply with python3
    
    Style changes:
      Restructured test_PBP_net.py script

--- c/test_PBP_net.py
+++ c/test_PBP_net.py
@@ -1,57 +1,68 @@
-
-import math
-
-import numpy as np
-
 import sys
-sys.path.append('PBP_net/')
-import PBP_net
+import math
+from pathlib import Path
+import numpy as np
+from PBP_net import PBP_net
 
-np.random.seed(1)
 
-# We load the boston housing dataset
+def load_data(datafile_path):
+    """Load dataset
+    Needs to be txt format with d columns
+    X: first d-1 columns
+    y: last column
+    """
 
-data = np.loadtxt('boston_housing.txt')
+    datafile_path = Path(datafile_path)
+    if datafile_path.exists():
+        data = np.loadtxt(datafile_path)
+    else:
+        print("Dataset file does not exist: {}".format(datafile_path))
+        sys.exit(1)
+    X = data[:, 0: data.shape[1]-1]
+    y = data[:, data.shape[1]-1]
+    return X, y
 
-# We obtain the features and the targets
 
-X = data[ :, range(data.shape[ 1 ] - 1) ]
-y = data[ :, data.shape[ 1 ] - 1 ]
+def create_data_split(X, y, proportion=0.9):
+    """Create data split
+    Default: Create the train and test sets with 90% and 10% of the data
+    """
 
-# We create the train and test sets with 90% and 10% of the data
+    np.random.seed(1)
+    permutation = np.random.choice(range(X.shape[0]),
+                                   X.shape[0], replace=False)
+    size_train = int(np.round(X.shape[0]*proportion))
+    index_train = permutation[0:size_train]
+    index_test = permutation[size_train:]
 
-permutation = np.random.choice(range(X.shape[ 0 ]),
-    X.shape[ 0 ], replace = False)
-size_train = np.round(X.shape[ 0 ] * 0.9)
-index_train = permutation[ 0 : size_train ]
-index_test = permutation[ size_train : ]
+    return X[index_train, :], y[index_train], X[index_test, :], y[index_test]
 
-X_train = X[ index_train, : ]
-y_train = y[ index_train ]
-X_test = X[ index_test, : ]
-y_test = y[ index_test ]
 
-# We construct the network with one hidden layer with two-hidden layers
-# with 50 neurons in each one and normalizing the training features to have
-# zero mean and unit standard deviation in the trainig set.
+def main():
+    """Main entry point for script"""
+    X, y = load_data("boston_housing.txt")
+    X_train, y_train, X_test, y_test = create_data_split(X, y, 0.9)
 
-n_hidden_units = 50
-net = PBP_net.PBP_net(X_train, y_train,
-    [ n_hidden_units, n_hidden_units ], normalize = True, n_epochs = 40)
+    # We construct the network with one hidden layer with two-hidden layers
+    # with 50 neurons in each one and normalizing the training features to have
+    # zero mean and unit standard deviation in the trainig set.
+    n_hidden_units = 50
+    n_epochs = 40
+    net = PBP_net.PBP_net(X_train, y_train, [n_hidden_units, n_hidden_units],
+                          normalize=True, n_epochs=n_epochs)
 
-# We make predictions for the test set
+    # We make predictions for the test set
+    m, v, v_noise = net.predict(X_test)
 
-m, v, v_noise = net.predict(X_test)
+    # We compute the test RMSE
+    rmse = np.sqrt(np.mean((y_test - m)**2))
+    print("RMSE: {}".format(rmse))
 
-# We compute the test RMSE
+    # We compute the test log-likelihood
+    test_ll = np.mean(-0.5 * np.log(2 * math.pi * (v + v_noise)) -
+                      0.5 * (y_test - m)**2 / (v + v_noise))
+    print("Log likelihood: {}".format(test_ll))
 
-rmse = np.sqrt(np.mean((y_test - m)**2))
 
-print rmse
-
-# We compute the test log-likelihood
-
-test_ll = np.mean(-0.5 * np.log(2 * math.pi * (v + v_noise)) - \
-    0.5 * (y_test - m)**2 / (v + v_noise))
-
-print test_ll
+if __name__ == "__main__":
+    main()
